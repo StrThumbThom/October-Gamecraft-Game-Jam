2d27931979c27d0fafe7f5933a627682398315fc
EAE-esports <EAE-esports@ad.utah.edu>
2017-10-20 22:24:52 -0600

    Making changes to Josh_Testing - working on procedural item/platform spawning

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour {
+
+    float xSpeed = 0.10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(this.transform.position.x - xSpeed, transform.position.y, transform.position.z);
+	}
+}
