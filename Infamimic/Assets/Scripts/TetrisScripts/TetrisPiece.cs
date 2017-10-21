da59a05ae85a11e7c469ff6c9a7520189ca1c7d5
Adam <adam.swaggoner@gmail.com>
2017-10-20 20:24:30 -0600

    Implement Blockshape

--- Infamimic/Assets/Scripts/TetrisScripts/TetrisPiece.cs
+++ Infamimic/Assets/Scripts/TetrisScripts/TetrisPiece.cs
@@ -2,11 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class BoolArray2D {
-    public bool[] array;
-}
-
 /*
  * Represents an inventory item that is a collection of squares.
  * Author: Adam Waggoner
@@ -18,15 +13,34 @@ public class TetrisPiece : MonoBehaviour {
     /// accessed by shape[y][x]
     /// </summary>
     [SerializeField]
-    private BoolArray2D[] shape;
+    private int _Shape;
+
+    [SerializeField]
+    private GameObject BlockSprite;
+
+    //bool matrix of whether block is present in shape
+    private bool[,] _ShapeGrid;
 
 	// Use this for initialization
 	void Start () {
-		
+        _ShapeGrid = new bool[4, 4];
+
+        int mask = 1;
+        for (int i = 0; i < 16; i++)
+        {
+            _ShapeGrid[i / 4, i % 4] = (mask & _Shape) != 0;
+            mask <<= 1;
+
+            if (_ShapeGrid[i / 4, i % 4])
+            {
+                var tmp = GameObject.Instantiate(BlockSprite, transform);
+                tmp.transform.localPosition = new Vector3(i / 4, i % 4, 0) * 100;
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.position = Input.mousePosition;
 	}
 }
